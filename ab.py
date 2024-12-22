87df5d2da192d8220bf6e5de9264b747b0a31234
MyDataversity-labs <syncdata759@gmail.com>
2024-12-22 16:52:17 +0530

    naah

--- ab.py
+++ ab.py
@@ -1,1 +1,2 @@
 dbutils.widgets.combobox(name='orderstatus',defaultValue='CLOSED',choices=['CLOSED','COMPLETE','PROCESSING'],label='ORDER STATUS')
+print("nhi nhi nhi")
---
+++ abb.py
@@ -0,0 +1,1 @@
+print("nhi nhi nhi")
